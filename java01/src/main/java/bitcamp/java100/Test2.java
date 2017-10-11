9f14c60a80b11089f2d3b8f087d50973506c7427
MIAE KIM <kma613@naver.com>
2017-10-11 17:54:45 +0900

    '.'

---
+++ java01/src/main/java/bitcamp/java100/Test2.java
@@ -0,0 +1,14 @@
+package bitcamp.java100;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+        System.out.print("쿠키");
+        System.out.print("까미");
+        System.out.println();
+
+        System.out.println("까미");
+        System.out.println("쿠키");
+    
+    }
+}
